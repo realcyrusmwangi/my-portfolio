410f5eb4f836f96961daa70a6c29411a2bb21502
realcyrusmwangi <realcyrusmwangi@gmail.com>
2025-08-15 17:18:06 +0300

    Update About.jsx

--- my-portfolio-frontend/src/About.jsx
+++ my-portfolio-frontend/src/About.jsx
@@ -265,7 +265,7 @@ export default function About() {
               Get In Touch
             </Link>
             <a
-              href="/cv/Cyrus_Mwangi_CV.pdf"
+              href="/cv/Cyrus_Mwangi_CV_NOCV.pdf"
               target="_blank"
               rel="noopener noreferrer"
               className="px-8 py-3 bg-white/10 backdrop-blur-sm border border-white/20 text-white font-semibold rounded-lg shadow-lg hover:shadow-blue-500/20 transition-all duration-500 hover:scale-105"
